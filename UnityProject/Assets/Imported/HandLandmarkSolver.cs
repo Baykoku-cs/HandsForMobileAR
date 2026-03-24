859fc7956fac7568292ecdc19c833d54072b3cf6
Baykoku-cs <Baykoku@yandex.ru>
2026-03-24 09:17:54 +0300

    Add my test code
    Hand landmarker and Hand visualizer above a ARFoundation. Now hands always spawn on the same distance (0.5f).

---
+++ UnityProject/Assets/Imported/HandLandmarkSolver.cs
@@ -0,0 +1,136 @@
+﻿using Mediapipe.Tasks.Vision.HandLandmarker;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using static UnityEngine.GraphicsBuffer;
+using static UnityEngine.XR.ARSubsystems.XRCpuImage;
+
+public class HandLandmarkSolver : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset model;
+    [SerializeField]
+    private ARCameraManager manager;
+    [SerializeField]
+    private Camera arCamera;
+    
+    [SerializeField]
+    private int targetFps = 30;
+    private float restTime;
+
+    [SerializeField]
+    private HandVisualizer handVisualizer;
+
+    private HandLandmarker handLandmarker;
+    
+    private Stopwatch stopwatch = new Stopwatch();
+    private HandLandmarkerResult _detectedLandmarks;
+    private ConversionParams? conversionParams;
+
+    private void Start()
+    {
+        restTime = 1 / targetFps;
+
+        handLandmarker = HandLandmarker.CreateFromOptions(new HandLandmarkerOptions(
+            new Mediapipe.Tasks.Core.BaseOptions(delegateCase: Mediapipe.Tasks.Core.BaseOptions.Delegate.CPU, 
+                                                 modelAssetBuffer: model.bytes),
+            Mediapipe.Tasks.Vision.Core.RunningMode.VIDEO
+        ));
+        
+        stopwatch.Start();
+    }
+
+    private bool IsReady = true;
+    private void FixedUpdate()
+    {
+        if (IsReady)
+        {
+            GenerateLandmarks();
+            StartCoroutine(Rest(1 / targetFps));
+        }
+    }
+
+    private IEnumerator Rest(float restTime)
+    {
+        IsReady = false;
+        yield return new WaitForSeconds(restTime);
+        IsReady = true;
+    }
+
+    private void GenerateLandmarks()
+    {
+        if (manager.TryAcquireLatestCpuImage(out XRCpuImage cpuImage))
+        {
+            _detectedLandmarks = handLandmarker.DetectForVideo(new Mediapipe.Image(GetTexture2DFromCpuImage(cpuImage)), stopwatch.ElapsedMilliseconds);
+
+            if (_detectedLandmarks.handLandmarks?.Count > 0)
+            {
+                List<Vector3> landmarkVectors = new();
+
+                for (int i = 0; i < _detectedLandmarks.handLandmarks[0].landmarks.Count; i++)
+                {
+                    Vector3 screenPoint = new Vector3(_detectedLandmarks.handLandmarks[0].landmarks[i].x * Screen.width, 
+                                                      _detectedLandmarks.handLandmarks[0].landmarks[i].y * Screen.height, 
+                                                      0.5f); 
+
+                    landmarkVectors.Add(arCamera.ScreenToWorldPoint(screenPoint));
+                }
+
+                handVisualizer.SendNewLandMarks(landmarkVectors.ToArray());
+
+                UnityEngine.Debug.Log("Landmarks placed in world space");
+            }
+            else
+            {
+                UnityEngine.Debug.Log("No hands detected");
+            }
+        }
+        else
+        {
+            UnityEngine.Debug.Log("No image");
+        }
+    }
+
+
+    private Texture2D GetTexture2DFromCpuImage(XRCpuImage cpuImage)
+    {
+        if (conversionParams is null)
+        {
+            conversionParams = new ConversionParams
+            {
+                inputRect = new RectInt(0, 0, cpuImage.width, cpuImage.height),
+
+                outputDimensions = new Vector2Int(cpuImage.width, cpuImage.height),
+
+                outputFormat = TextureFormat.RGBA32,
+
+                transformation = Transformation.MirrorX
+            };
+        }
+
+        var texture = new Texture2D(cpuImage.width, cpuImage.height, TextureFormat.RGBA32, false);
+        try
+        {
+            unsafe
+            {
+                cpuImage.Convert(
+                  conversionParams.Value,
+                  new IntPtr(texture.GetRawTextureData<byte>().GetUnsafePtr()),
+                  texture.GetRawTextureData<byte>().Length);
+            }
+        }
+        finally
+        {
+            cpuImage.Dispose();
+        }
+
+        texture.Apply();
+
+        return texture;
+    }
+}
---
+++ UnityProject/Assets/Imported/HandVisualizer.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HandVisualizer : MonoBehaviour
+{
+    [SerializeField]
+    private Transform worldParent;
+    private Rigidbody[] spheres = new Rigidbody[21];
+    private Vector3[] lastTrakedLandmarkVectors = new Vector3[21];
+
+    [SerializeField]
+    private Rigidbody handPointPrefab;
+
+    private bool isRecentlyUpdated = false;
+    private Coroutine hideHandsDelayedCoroutine;
+    
+    private void Start()
+    {
+        for (int i = 0; i < 21; i++)
+        {
+            spheres[i] = Instantiate(handPointPrefab);
+            spheres[i].gameObject.SetActive(false);
+            spheres[i].transform.SetParent(worldParent);
+            spheres[i].transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isRecentlyUpdated)
+        {
+            for (int i = 0; i < 21; i++)
+            {
+                if (i == 0)
+                {
+                    worldParent.position = lastTrakedLandmarkVectors[i];
+                }
+
+                if (isRecentlyUpdated)
+                {
+                    spheres[i].MovePosition(lastTrakedLandmarkVectors[i]);
+
+                }
+                else
+                {
+                    spheres[i].transform.position = lastTrakedLandmarkVectors[i];
+                }
+            }
+        }
+    }
+
+    public void SendNewLandMarks(Vector3[] landmarkVectors)
+    {
+        for (int i = 0; i < landmarkVectors.Length; i++)
+        {
+            lastTrakedLandmarkVectors[i] = landmarkVectors[i];
+
+            if (!isRecentlyUpdated)
+            {
+                spheres[i].gameObject.SetActive(true);
+            }
+        }
+        isRecentlyUpdated = true;
+
+        if (hideHandsDelayedCoroutine != null)
+            StopCoroutine(hideHandsDelayedCoroutine);
+
+        hideHandsDelayedCoroutine = StartCoroutine(DelayedHideHands());
+    }
+
+    private IEnumerator DelayedHideHands()
+    {
+        yield return new WaitForSeconds(3);
+        isRecentlyUpdated = false;
+        HideHands();
+    }
+    private void HideHands()
+    {
+        foreach (var sphere in spheres)
+        {
+            sphere.gameObject.SetActive(false);
+        }
+    }
+}
