fbfa217ba3b6843f3ee6f2989e4142a777c5bc7d
Baykoku-cs <Baykoku@yandex.ru>
2026-04-09 01:14:11 +0300

    First version of vr mode
    Needs to be added fish-eye effect
    + eye's images should be reduced in size

---
+++ UnityProject/Assets/Scripts/VrMode.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VrMode : MonoBehaviour
+{
+    [SerializeField] private Camera CameraToSplit;
+    [SerializeField] private RawImage LeftEye;
+    [SerializeField] private RawImage RightEye;
+    [SerializeField] private bool TriggerSplitModeOn;
+    
+    private float _cropPercentage = 0.25f;
+    private bool IsSplitModeOn;
+    private RenderTexture renderTexture;
+    private RenderTexture originalTargetTexture;
+
+    private void Start()
+    {
+        originalTargetTexture = CameraToSplit.targetTexture;
+    }
+
+    private void Update()
+    {
+        if (!TriggerSplitModeOn.Equals(IsSplitModeOn))
+        {
+            IsSplitModeOn = TriggerSplitModeOn;
+            ChangeMode(IsSplitModeOn);
+        }
+    }
+
+    private void ChangeMode(bool currentMode)
+    {
+        var currentScreenRect = GetComponent<RectTransform>();
+        var lEyeRect = LeftEye.gameObject.GetComponent<RectTransform>();
+        var rEyeRect = RightEye.gameObject.GetComponent<RectTransform>();
+
+        if (currentMode)
+        {
+            EnableRenderTextureMode();
+            var texture = renderTexture;
+
+            float textureAspect = (float)texture.width / texture.height;
+            float screenAspect = currentScreenRect.rect.width / currentScreenRect.rect.height;
+
+            float eyeWidth;
+
+            eyeWidth = currentScreenRect.rect.width * 0.5f;
+
+            lEyeRect.sizeDelta = new Vector2(eyeWidth, lEyeRect.sizeDelta.y);
+            rEyeRect.sizeDelta = new Vector2(eyeWidth, rEyeRect.sizeDelta.y);
+
+            LeftEye.texture = texture;
+            RightEye.texture = texture;
+
+            LeftEye.enabled = true;
+            RightEye.enabled = true;
+}
+        else
+        {
+            DisableRenderTextureMode();
+            LeftEye.enabled = false;
+            RightEye.enabled = false;
+        }
+    }
+
+
+    private void EnableRenderTextureMode()
+    {
+        renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
+
+        originalTargetTexture = CameraToSplit.targetTexture;
+        CameraToSplit.targetTexture = renderTexture;
+    }
+
+    private void DisableRenderTextureMode()
+    {
+        CameraToSplit.targetTexture = originalTargetTexture;
+
+        if (renderTexture != null)
+        {
+            renderTexture.Release();
+            renderTexture = null;
+        }
+    }
+
+    public void ChangeMode()
+    {
+        TriggerSplitModeOn = !TriggerSplitModeOn;
+    }
+}
